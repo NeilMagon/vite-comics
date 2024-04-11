134884ff159586ea2b9317e0c9482f58d94da950
NeilMagon <neil.magon97@gmail.com>
2024-04-11 22:38:33 +0200

    feat: creation AppFooter

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -3,3 +3,26 @@ export default {
     name: 'AppFooter',
 }
 </script>
+
+<template>
+    <section>
+        <div class="container">
+            <div class="d-flex justify-content-between align-items-center py-4">
+                <div>
+                    <button>SIGN-UP-NOW</button>
+                </div>
+                <div class="d-flex align-items-center">
+                    <div class="text-white">FOLLOW US</div>
+                </div>
+            </div>
+        </div>
+    </section>
+</template>
+
+<style scoped lang="scss">
+    @use '../style/partials/variables' as *;
+
+    section{
+        background-color: $brand-secondary-light;
+    }
+</style>
