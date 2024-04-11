4be42e0575efbb221d598fba9f386a768f9d8f58
NeilMagon <neil.magon97@gmail.com>
2024-04-11 17:56:30 +0200

    feat: create and collegate all components. Add bootstrap

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,35 @@
 <script>
-export default {
-  
-}
+  import AppHeader from './components/AppHeader.vue';
+  import AppHero from './components/AppHero.vue';
+  import AppIcons from './components/AppIcons.vue';
+  import AppInfo from './components/AppInfo.vue';
+  import AppFooter from './components/AppFooter.vue';
+
+  export default {
+    components: {
+      AppHeader,
+      AppHero,
+      AppIcons,
+      AppInfo,
+      AppFooter
+    }
+  }
 </script>
 
 <template>
+  <header>
+    <AppHeader></AppHeader>
+  </header>
 
+  <main>
+    <AppHero></AppHero>
+    <AppIcons></AppIcons>
+    <AppInfo></AppInfo>
+  </main>
+  
+  <footer>
+    <AppFooter></AppFooter>
+  </footer>
 </template>
 
 <style>
---
+++ src/components/AppIcons.vue
@@ -0,0 +1,5 @@
+<script>
+export default {
+    name: 'AppIcons',
+}
+</script>
