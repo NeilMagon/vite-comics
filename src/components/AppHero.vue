7808c594ed5c3dbe2040110b45d5575688cb3b2c
NeilMagon <neil.magon97@gmail.com>
2024-04-11 19:04:00 +0200

    feat: add hero

--- src/components/AppHero.vue
+++ src/components/AppHero.vue
@@ -3,3 +3,21 @@ export default {
     name: 'AppHero',
 }
 </script>
+
+<template>
+    <section class="ms-black">
+        <div class="container">
+            <h1 class="ms-white">Content goes here</h1>
+        </div>
+    </section>
+</template>
+
+<style scoped lang="scss">
+    .ms-black{
+        background-color: black;
+        height: 200px;
+    }
+    .ms-white{
+        color: #fff;
+    }
+</style>
